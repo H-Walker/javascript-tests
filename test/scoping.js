949745463eb401baf635bb025b6934c55b86edfc
H-Walker <47987019+H-Walker@users.noreply.github.com>
2019-03-04 19:44:13 +0000

    Update scoping.js

--- test/scoping.js
+++ test/scoping.js
@@ -3,8 +3,8 @@ describe('scoping', function () {
     var mod = new Module(),
         request;
 
-    request = function (callback) {
-      return eval('this.' + callback)();
+    request = function (callback,obj) {
+      return callback.call(obj);
     };
 
     function Module () {
@@ -16,7 +16,7 @@ describe('scoping', function () {
     };
 
     Module.prototype.req = function() {
-      return request(this.method);
+      return request(this.method,this);
     };
 
     expect(mod.req()).toBe('bar');
